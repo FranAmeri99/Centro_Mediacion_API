1d09b0ebef2d7d1807967c63489fdbe8dd1e0c46
FranAmeri99 <franameri2016@gmail.com>
2021-11-03 14:52:31 -0300

    Super User

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -14,6 +14,41 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
+    def test_create_user_all_data(self):
+        """Test creating a user complet"""
+        email = 'test@francisco.com'
+        password = 'Password99?'
+        name = 'francisco'
+        last_name = 'Ameri'
+        gender = 'male'
+        dni = 11111111
+        nationality = 'Argentino'
+        birth_date = '18/02/1999'
+        username = 'FranAmeri'
+        enrollment = '1111'
+
+        user = get_user_model().objects.create_user(
+        email=email, 
+        password=password,
+        name=name,
+        last_name=last_name,
+        gender=gender,
+        dni=dni,
+        nationality =nationality,
+        birth_date=birth_date,
+        username=username,
+        enrollment=enrollment
+        )
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.name, name)
+        self.assertEqual(user.last_name, last_name)
+        self.assertEqual(user.gender, gender)
+        self.assertEqual(user.dni, dni)
+        self.assertEqual(user.nationality, nationality)
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.enrollment, enrollment)
+        self.assertTrue(user.check_password(password))
+
     def test_user_email(self):
         email = 'test@FRANCISCO.com'
         user =get_user_model().objects.create_user(email, 'test123')
@@ -35,3 +70,4 @@ class ModelTests(TestCase):
             
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+    
